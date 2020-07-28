64114d3c9ca852591de2422496ec697d5728e414
Roger Winright <Roger@Williams-MacBook-Pro.local>
2020-07-28 01:45:30 -0400

    initial commit

--- export/neko/haxe/ApplicationMain.hx
+++ export/neko/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "112";
+		app.meta["build"] = "1";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "Plat-Test";
 		app.meta["name"] = "Plat-Test";
